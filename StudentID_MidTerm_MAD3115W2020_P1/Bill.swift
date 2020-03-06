8bdf388572b395bba81c61b7550c8ef925160f4e
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-06 08:46:01 -0500

    [AN] Added bill class code , used previous code of my project

--- StudentID_MidTerm_MAD3115W2020_P1/Bill.swift
+++ StudentID_MidTerm_MAD3115W2020_P1/Bill.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import Foundation
+enum BillType{
+    case Mobile, Internet, Hydro, Insurance
+}
+
+class Bill:IDisplay{
+    
+     var billId: String
+     var billDate: Date
+     var billType: BillType?
+     var billAmount: Double
+    
+    init(billId: String,billDate: Date,billType: BillType, billAmount: Double) {
+        self.billId = billId
+        self.billDate = billDate
+        self.billType = billType
+        self.billAmount = billAmount
+    }
+    func display() {
+        
+        print("         Bill ID : \(billId)")
+        let str = billDate.formatDate(date: billDate)
+        print("         Bill Date : \(str)")
+        print("         Bill Type : \(billType!)")
+        let d = "".formatCurrency(billAmount: billAmount)
+        print("         Bill Amount : \(d)")
+        
+    }
+  }
