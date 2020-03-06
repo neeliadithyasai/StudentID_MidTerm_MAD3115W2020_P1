44da0eab56eadcdef5f3a654a537787fa7967ba5
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-06 08:43:11 -0500

    [AN]Added customer class code which is referred from our previous project

--- StudentID_MidTerm_MAD3115W2020_P1/Customer.swift
+++ StudentID_MidTerm_MAD3115W2020_P1/Customer.swift
@@ -9,3 +9,55 @@
 
 
 import Foundation
+
+enum validEmail: Error{
+    case invalidEmail(Problem : String)
+}
+
+class Customer: IDisplay {
+    var customerId: String
+    var firstName: String
+    var lastName: String
+    var fullName: String{
+        return "\(firstName) \(lastName)"
+    }
+    var email: String
+    lazy var bills : [String:Bill]=[:]
+    lazy var totalBill: Double = calculatedBill()
+    
+
+    init(customerId:String, firstName: String, lastName: String, email: String)throws {
+        self.customerId = customerId
+        self.firstName = firstName
+        self.lastName = lastName
+        
+        guard email.isValidEmail else {
+            throw validEmail.invalidEmail(Problem: "\(email)")
+        }
+    
+        self.email = email
+    }
+    func calculatedBill() -> Double{
+        var t = 0.0
+        for(_,v) in bills{
+            t = t + v.billAmount
+        }
+        return t
+    }
+    
+    func display(){
+        print("Customer ID : \(customerId)")
+        print("Customer Full Name : \(fullName)")
+        if email.isValidEmail{
+            print("Customer Email ID : \(email)")}
+        print("         ------------Bill Information-------------    ")
+        print("         ***************************************************")
+        for bill in bills{
+            bill.value.display()
+        }
+        let t = "".formatCurrency(billAmount: calculatedBill())
+        print("                  Total Bill Amount : \(t)")
+        print("         ***************************************************")
+        print(" ")
+    }
+  }
