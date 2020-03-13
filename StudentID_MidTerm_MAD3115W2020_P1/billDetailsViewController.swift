70c3f96d68dc0511ba54c10733f7cff231ebf1ea
ADITHYA SAI <60120972+neeliadithyasai@users.noreply.github.com>
2020-03-13 17:56:19 -0400

    [AN] removed necessary line in remote view controller

--- StudentID_MidTerm_MAD3115W2020_P1/billDetailsViewController.swift
+++ StudentID_MidTerm_MAD3115W2020_P1/billDetailsViewController.swift
@@ -12,9 +12,7 @@ class billDetailsViewController: UIViewController {
 
     @IBOutlet weak var customerInfo: UILabel!
 
-    @IBOutlet weak var btnViewBills: UIButton!
-    
-    @IBOutlet weak var lblBillDetails: UILabel!
+
     @IBOutlet weak var tblBillsView: UITableView!
     
     
@@ -35,26 +33,7 @@ class billDetailsViewController: UIViewController {
             customerInfo.backgroundColor = UIColor.white
             customerInfo.textColor = UIColor.black
         }
-          
-//           if let id = customerBill?.customerId, let fn = customerBill?.fullName, let em = customerBill?.email
-//           {
-//               customerInfo.text = "Customer ID : \(id)\nCustomer Name : \(fn)\nCustomer Email : \(em)"
-//               customerInfo.numberOfLines = 0
-//
-//              if let abc = customerBill?.bills
-//              {
-//                if abc.count > 0
-//                {
-//                   for(_,v) in abc
-//                   {
-//                    lblBillDetails.text = " Bill ID : \(v.billId)\n Bill Type: \(v.billType!)\n Bill Date :\(v.billDate)\n Bill Amount:\(v.billAmount) "
-//                    lblBillDetails.numberOfLines = 0
-//
-//                  }
-//               }
-//
-//
-//            }
+
      }
     override func viewWillAppear(_ animated: Bool) {
            super.viewWillAppear(animated)
@@ -80,88 +59,55 @@ class billDetailsViewController: UIViewController {
     
 }
    
-//    @IBAction func btnActnViewBills(_ sender: UIButton) {
-//
-//        let sb = UIStoryboard(name: "Main", bundle: nil)
-//
-//        if let viewController = sb.instantiateViewController(identifier: "billsView") as? billsViewController {
-//            navigationController?.pushViewController(viewController, animated: true)
-//    }
-    
 
-    //}
 
 extension billDetailsViewController: UITableViewDelegate, UITableViewDataSource{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        3
+        1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let label = UILabel()
-        if section == 0{
-            label.text = "Internet"
-            label.backgroundColor = UIColor.systemRed
-        }
-        else if section == 1 {
-            label.text = "Hydro"
-            label.backgroundColor = UIColor.systemRed
-        }
-        else {
-            label.text = "Mobile"
-            label.backgroundColor = UIColor.systemRed
-        }
-        return label
+        return (customerBill?.getBills().count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "billCell", for: indexPath)
-       
+        let bill = Bills[indexPath.row]
+        if bill.billId.contains("INT"){
         
-        if indexPath.section == 0{
+        //if indexPath.section == 0{
+                let str = bill.billDate.formatDate()
+                 cell.textLabel?.text = "Bill Id : \(bill.billId)\nBill Type : Internet\nBill Date : \(str)\nBill Amount : \(bill.billAmount)"
+                 cell.textLabel?.numberOfLines = 0
+
+       // }
+        }
+        if bill.billId.contains("HYD"){
+        // if indexPath.section == 1{
+                
+                    let str = bill.billDate.formatDate()
+                    cell.textLabel?.text = "Bill Id : \(bill.billId)\nBill Type : Hydro\nBill Date : \(str)\nBill Amount : \(bill.billAmount)"
+                    cell.textLabel?.numberOfLines = 0
+
+
+        }
+       // }
+        if bill.billId.contains("MOB"){
+       // if indexPath.section == 2
+         //{
             
-            let exists = customerBill?.bills["I"] != nil
-            if exists{
-                let v = customerBill?.bills["I"]
-                let str = v?.billDate.formatDate()
-                cell.textLabel?.text = "Bill Id : \(v!.billId)\nBill Type : Internet\nBill Date : \(str!)\nBill Amount : \(v!.billAmount)"
-                cell.textLabel?.numberOfLines = 0
-            }
-            else {
-                cell.textLabel?.text = "No bill"
-            }
+                let str = bill.billDate.formatDate()
+                 cell.textLabel?.text = "Bill Id : \(bill.billId)\nBill Type : Mobile\nBill Date : \(str)\nBill Amount : \(bill.billAmount)"
+                 cell.textLabel?.numberOfLines = 0
+
+            
+
         }
-        if indexPath.section == 1{
-            let exists = customerBill?.bills["H"] != nil
-            if exists{
-                let v = customerBill?.bills["H"]
-                let str = v?.billDate.formatDate()
-                cell.textLabel?.text = "Bill Id : \(v!.billId)\nBill Type : Hydro\nBill Date : \(str!)\nBill Amount : \(v!.billAmount)"
-                cell.textLabel?.numberOfLines = 0
-            }
-            else {
-                cell.textLabel?.text = "No bill"
-            }
-        }
-        
-        if indexPath.section == 2
-        {
-            let exists = customerBill?.bills["M"] != nil
-            if exists{
-                let v = customerBill?.bills["M"]
-                let str = v?.billDate.formatDate()
-                cell.textLabel?.text = "Bill Id : \(v!.billId)\nBill Type : Mobile\nBill Date : \(str!)\nBill Amount : \(v!.billAmount)"
-                cell.textLabel?.numberOfLines = 0
-            }
-            else {
-                cell.textLabel?.text = "No bill"
-            }
-        }
-        return cell
+     
+            return cell
+    
+
         
     }
 }
